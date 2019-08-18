4bee5c1f3af093b8335f38275a1a7d8828b27ca3
Remigiusz <remigiuszwyciszkiewicz000@gmail.com>
2019-08-18 19:15:29 +0200

    main router module

--- apps/cypto-research/src/app/app.module.ts
+++ apps/cypto-research/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MainListModule } from '@myorg/crypto/feature/main-list'
+import { MainListModule } from '@myorg/crypto/feature/main-list';
 import { AppComponent } from './app.component';
-import { DataAccessModule } from '@myorg/shared/data-access'
+import { DataAccessModule } from '@myorg/shared/data-access';
+import { AppRoutingModule } from './app-routing.module';
+
+const imports = [
+  BrowserModule,
+  MainListModule,
+  DataAccessModule,
+  AppRoutingModule,
+  AppRoutingModule
+];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MainListModule, DataAccessModule],
+  imports: [...imports],
   providers: [],
   bootstrap: [AppComponent]
 })
