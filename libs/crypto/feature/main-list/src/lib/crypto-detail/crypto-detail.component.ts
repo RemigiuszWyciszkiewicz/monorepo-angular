9bd149f18008c1aca722c4cfeb04cb5ef98807d6
Remigiusz <remigiuszwyciszkiewicz000@gmail.com>
2019-08-21 18:06:54 +0200

    details component

--- libs/crypto/feature/main-list/src/lib/crypto-detail/crypto-detail.component.ts
+++ libs/crypto/feature/main-list/src/lib/crypto-detail/crypto-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Route, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CryptocurrencyDetails } from 'libs/crypto/data-access/models/cryptocurrency-details';
+import { RestService } from 'libs/shared/data-access/src/lib/services/rest-service';
 
 @Component({
   selector: 'myorg-crypto-detail',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptoDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private rotue: ActivatedRoute, private restService: RestService) { }
+
+  cryptocurrencyDetails$: Observable<CryptocurrencyDetails>;
+  cryptocurrencytSymbol: string;
 
   ngOnInit() {
+    this.rotue.paramMap.subscribe(val =>
+      this.cryptocurrencytSymbol=val.get('id')
+      );
+
+    this.restService.getCryptocurrencyDetais(this.cryptocurrencytSymbol).subscribe( val => this.cryptocurrencyDetails$ = val);
+  }
+
+  goBack() {
+    this.router.navigate(['../'], { relativeTo: this.rotue});
   }
 
 }
--- libs/crypto/feature/main-list/src/lib/main-list-routing.module.ts
+++ libs/crypto/feature/main-list/src/lib/main-list-routing.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
+import { CryptoDetailComponent } from './crypto-detail/crypto-detail.component';
+import { MainListComponent } from './main-list.component';
 
-
-const routes: Routes = [ { path: '', component: CryptoListComponent }]
+const routes: Routes = [
+  {
+    path: '',
+    component: MainListComponent,
+    children: [
+      {
+        path: '',
+        component: CryptoListComponent
+      },
+      { path: ':id', component: CryptoDetailComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MainListRoutingModule { }
+export class MainListRoutingModule {}
